e414e16264148922b75fd0669fd8942ba58bd884
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-11-09 16:33:29 +0530

    onboarding page completed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { Link } from 'expo-router'
+import { Redirect,router } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants'
+import CustomButton from '@/components/CustomButton'
+
 
 const index = () => {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-3xl'>Aora!</Text>
-      <StatusBar style='auto' />
-      <Link href={'/home'} className='text-blue-500 text-xl'>Go to Home</Link>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{height:'100%'}}>
+        <View className='w-full items-center justify-center h-full px-4'>
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode='contain'
+          />
+          <Image
+            source={images.cards}
+            className='max-w-[380px] w-full h-[300px]'
+            resizeMode='contain'
+          />
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center'>Discover Endless Possibilities with{' '}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+              resizeMode='contain'
+            />
+          </View>
+          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {router.push('/sign-in')}}
+            containerStyles='mt-7 w-full' 
+            textStyles={undefined}
+            isLoading={undefined}            
+          />
+        </View>
+      </ScrollView>
+      <StatusBar style='light' backgroundColor='#161622'/>
+    </SafeAreaView>
   )
 }
 
